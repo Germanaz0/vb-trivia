c135dc2b5df73b9659d2f2c353d41b13b374be55
Germanaz0 <germanazo@gmail.com>
2013-11-13 23:13:37 -0300

    add database

--- db/vb-trivia.sql
+++ db/vb-trivia.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 13, 2013 at 07:59 PM
+-- Generation Time: Nov 13, 2013 at 11:12 PM
 -- Server version: 5.5.34-0ubuntu0.13.10.1
 -- PHP Version: 5.5.3-1ubuntu2
 
@@ -29,17 +29,26 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `preguntas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pregunta` text NOT NULL,
-  `resputesta` text NOT NULL,
+  `respuesta` text NOT NULL,
   `puntaje` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `preguntas`
 --
 
-INSERT INTO `preguntas` (`id`, `pregunta`, `resputesta`, `puntaje`) VALUES
-(1, 'De que gusto es la sal ?', 'Salado', 200);
+INSERT INTO `preguntas` (`id`, `pregunta`, `respuesta`, `puntaje`) VALUES
+(4, '¿En qué deporte se usa tiza?.', 'Pool', 5),
+(5, '¿Cuántas manos tiene un caballo? ( en número )', '2', 5),
+(6, '¿Qué instrumento musical tiene nombre y forma geométricos?', 'triangulo', 5),
+(7, '¿Cuáles son las dos primeras palabras de la Biblia?', 'Al Principio', 15),
+(8, '¿Quién escribió "El Diario de Ana Frank"?', 'Ana Frank', 20),
+(9, '¿Cómo se llaman las crías de la mula?', 'No tiene', 1),
+(10, '¿Cuál es el limite de edad establecido para participar en los Juegos Olímpicos? (en número)', '0', 20),
+(11, '¿Qué isla del Caribe tiene nombre de flor?', 'Isla Margarita', 15),
+(12, '¿Qué capital española tiene cinco veces la misma vocal "a"?', 'Guadalajara', 10),
+(13, '¿Cuántas capitales de provincia españolas empiezan por "V"? (en número)', '3', 20);
 
 -- --------------------------------------------------------
 
@@ -53,7 +62,14 @@ CREATE TABLE IF NOT EXISTS `resultados` (
   `resultado` int(11) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `resultados`
+--
+
+INSERT INTO `resultados` (`id`, `usuario_id`, `resultado`, `created_at`) VALUES
+(7, 1, 36, '2013-11-14 02:12:17');
 
 -- --------------------------------------------------------
 
